686eced48f1027f879e2db621321f34059e62d98
EddieKarugu <gichukiedwin36@gmail.com>
2025-10-29 09:38:08 +0300

    Enabled filters

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:phanstore/widgets/custom_input_field.dart';
 import 'package:phanstore/widgets/product_container.dart';
-
 import '../dummy_data.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,11 +13,13 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   int activeTile = 0;
   late TextEditingController searchController;
+  late List<Map<String, dynamic>> displayProducts;
 
   @override
   void initState() {
     super.initState();
     searchController = TextEditingController();
+    displayProducts = products;
   }
 
   @override
@@ -28,145 +29,169 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            isWideScreen
-                ? const SizedBox()
-                : ShaderMask(
-                    shaderCallback: (Rect bounds) {
-                      return LinearGradient(
-                        colors: [
-                          Theme.of(context).colorScheme.primary,
-                          Colors.deepPurple,
-                          Theme.of(context).colorScheme.secondary,
-                        ],
-                      ).createShader(bounds);
-                    },
-                    blendMode: BlendMode.srcIn,
-                    child: FittedBox(
-                      child: Text(
-                        'PHANSTORE',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 40,
-                          letterSpacing: 6,
-                        ),
-                      ),
-                    ),
-                  ),
-            Row(
-              mainAxisAlignment: isWideScreen
-                  ? MainAxisAlignment.center
-                  : MainAxisAlignment.spaceBetween,
-              children: [
-                isWideScreen
-                    ? Container(
-                        constraints: BoxConstraints(maxWidth: 600),
-                        child: CustomInputField(
-                          icon: Icons.search,
-                          hint: 'Search Food items here',
-                          controller: searchController,
-                          obscureText: false,
-                        ),
-                      )
-                    : Expanded(
-                        child: CustomInputField(
-                          icon: Icons.search,
-                          hint: 'Search Food items here',
-                          controller: searchController,
-                          obscureText: false,
-                        ),
-                      ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: InkWell(
-                    onTap: () {},
-                    splashColor: Theme.of(context).colorScheme.primary,
-                    borderRadius: BorderRadius.circular(12),
-                    child: Container(
-                      margin: const EdgeInsets.all(3),
-                      padding: const EdgeInsets.all(4),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.primary,
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: const Icon(Icons.search, size: 40),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 70,
-              child: ListView.builder(
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 12.0,
-                      horizontal: 4,
-                    ),
-                    child: ActionChip(
-                      label: Row(
-                        children: [
-                          Image.asset(
-                            'assets/images/${tiles[index]['image']}',
-                            width: 30,
-                            height: 30,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 8.0,
-                            ),
-                            child: Text(tiles[index]['name']),
-                          ),
-                        ],
-                      ),
-                      backgroundColor: activeTile == index
-                          ? Theme.of(context).colorScheme.primary
-                          : null,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        side: BorderSide(
-                          color: Theme.of(context).colorScheme.primary,
-                          width: 1,
-                        ),
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          activeTile = index;
-                        });
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              isWideScreen
+                  ? const SizedBox()
+                  : ShaderMask(
+                      shaderCallback: (Rect bounds) {
+                        return LinearGradient(
+                          colors: [
+                            Theme.of(context).colorScheme.primary,
+                            Colors.deepPurple,
+                            Theme.of(context).colorScheme.secondary,
+                          ],
+                        ).createShader(bounds);
                       },
+                      blendMode: BlendMode.srcIn,
+                      child: FittedBox(
+                        child: Text(
+                          'PHANSTORE',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 40,
+                            letterSpacing: 6,
+                          ),
+                        ),
+                      ),
                     ),
-                  );
-                },
-                itemCount: tiles.length,
-                scrollDirection: Axis.horizontal,
+              Row(
+                mainAxisAlignment: isWideScreen
+                    ? MainAxisAlignment.center
+                    : MainAxisAlignment.spaceBetween,
+                children: [
+                  isWideScreen
+                      ? Container(
+                          constraints: BoxConstraints(maxWidth: 600),
+                          child: CustomInputField(
+                            icon: Icons.search,
+                            hint: 'Search Food items here',
+                            controller: searchController,
+                            obscureText: false,
+                          ),
+                        )
+                      : Expanded(
+                          child: CustomInputField(
+                            icon: Icons.search,
+                            hint: 'Search Food items here',
+                            controller: searchController,
+                            obscureText: false,
+                          ),
+                        ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: InkWell(
+                      onTap: () {
+                      },
+                      splashColor: Theme.of(context).colorScheme.primary,
+                      borderRadius: BorderRadius.circular(12),
+                      child: Container(
+                        margin: const EdgeInsets.all(3),
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).colorScheme.primary,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: const Icon(Icons.search, size: 40),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            const SizedBox(height: 20),
-            Expanded(
-              child: LayoutBuilder(
-                builder: (context, constraints) {
-                  final bool isWideScreen = constraints.maxWidth > 1000;
-                  final bool isTablet =
-                      constraints.maxWidth > 600 && constraints.maxWidth < 1000;
+              SizedBox(
+                height: 70,
+                child:
+                ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 12.0,
+                        horizontal: 4,
+                      ),
+                      child: ActionChip(
+                        label: Row(
+                          children: [
+                            Image.asset(
+                              'assets/images/${tiles[index]['image']}',
+                              width: 30,
+                              height: 30,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 8.0,
+                              ),
+                              child: Text(tiles[index]['name']),
+                            ),
+                          ],
+                        ),
+                        backgroundColor: activeTile == index
+                            ? Theme.of(context).colorScheme.primary
+                            : null,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                          side: BorderSide(
+                            color: Theme.of(context).colorScheme.primary,
+                            width: 1,
+                          ),
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            activeTile = index;
+                            activeTile == 0? displayProducts = products
+                                :displayProducts = products
+                                .where(
+                                  (product) =>
+                              product['category']
+                                  .toString()
+                                  .toLowerCase() ==
+                                  tiles[activeTile]['name']
+                                      .toString()
+                                      .toLowerCase(),
+                            )
+                                .toList();
+                          });
+                        },
+                      ),
+                    );
+                  },
+                  itemCount: tiles.length,
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    final bool isWideScreen = constraints.maxWidth > 1000;
+                    final bool isTablet =
+                        constraints.maxWidth > 600 &&
+                        constraints.maxWidth < 1000;
 
-                  return GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: isWideScreen ? 8 : isTablet ? 6 : 3,
-                      crossAxisSpacing: 4,
-                      childAspectRatio: .72,
-                      mainAxisSpacing: 4,
-                    ),
-                    itemBuilder: (context, index) {
-                      return ProductContainer(product: products[index]);
-                    },
-                    itemCount: products.length,
-                  );
-                },
+                    return displayProducts.isEmpty? const Text('No Such Items Found'):GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: isWideScreen
+                            ? 8
+                            : isTablet
+                            ? 6
+                            : 3,
+                        crossAxisSpacing: 4,
+                        childAspectRatio: .72,
+                        mainAxisSpacing: 4,
+                      ),
+                      itemBuilder: (context, index) {
+                        return ProductContainer(
+                          product: displayProducts[index],
+                        );
+                      },
+                      itemCount: displayProducts.length,
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
